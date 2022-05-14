75ada3093a8936c5401dfc512e56f04f09b174db
oyesa <mercyoyesa2018@gmail.com>
2022-05-14 17:32:51 +0300

    create index and category html templates

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from turtle import title
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Welcome to the best pitch website!'
+    return render_template('index.html', title = title)
+
+@app.route('/category/<int:add_category_id>')
+def add_category(add_category_id):
+
+    '''
+    View movie page function that returns the add category details page and its data
+    '''
+    return render_template('add_category.html',id = add_category_id)
