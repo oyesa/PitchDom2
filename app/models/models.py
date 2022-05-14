056d531da003569407738de8e8562a1febb27303
oyesa <mercyoyesa2018@gmail.com>
2022-05-14 17:49:28 +0300

    create models.py file, install flask login

---
+++ app/models/models.py
@@ -0,0 +1,20 @@
+from flask_login import login_manager
+from flask_login import UserMixin
+
+
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
+
+# class User(UserMixin,db.Model): 
+#     __tablename__ = 'users' 
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(255),index = True)
+#     email = db.Column(db.String(255),unique = True,index = True)
+#     comments = db.relationship('Comment', backref='author',lazy="dynamic")
+#     bio = db.Column(db.String(255))
+#     profile_pic_path = db.Column(db.String())
+#     pass_secure = db.Column(db.String(255))
+#     pitches = db.relationship('Pitch',backref = 'pitcher',lazy = "dynamic")
