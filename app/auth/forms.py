ec87240b4823fc594cf01d5367899ab3e986e7e9
oyesa <mercyoyesa2018@gmail.com>
2022-05-15 11:25:27 +0300

    update auth forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,23 +5,31 @@ from wtforms import StringField, BooleanField, PasswordField, SubmitField, Valid
 
 
 
-class SignUpForm(FlaskForm):
-    email = StringField('Enter Email Address',validators=[DataRequired(),Email()])
-    username = StringField('Enter Username',validators = [DataRequired(),Length(min=2,max=20)])
-    password = PasswordField('Password',validators = [DataRequired()])
-    password_confirm = PasswordField('Confirm Password',validators = [DataRequired(), EqualTo('password',message = 'Passwords must match')])
-    submit = SubmitField('Sign Up')
-
-    def validate_email(self,data_field):
-            if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('An account already exists with that email')
-
-    def validate_username(self,data_field):
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('Username is taken')
-
 class LoginForm(FlaskForm):
-    email = StringField('Enter Email Address',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators =[DataRequired()])
-    remember = BooleanField('Remember Me') 
-    submit = SubmitField('Sign In')
+    """
+    Login form
+    """
+    email = StringField('Email', validators=[DataRequired(), Length(1, 50), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    """
+     class that defines the registration form
+    """
+    name = StringField('Name', validators=[DataRequired(), Length(1, 50)])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 50), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 50)])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match.')])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Create Account')
+
+    def check_email_exist(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered. Please Login')
+
+    def check_username_exist(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already exists.')
